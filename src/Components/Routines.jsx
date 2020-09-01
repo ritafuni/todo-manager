f4733f51e794d26d09261d7122773d1937e07592
ritafuni <amarettoh@gmail.com>
2020-09-01 23:51:11 +0900

    made basic radio buttons for routines2

--- src/Components/Routines.jsx
+++ src/Components/Routines.jsx
@@ -6,7 +6,6 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
 const Container = styled.div`
