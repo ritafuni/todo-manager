c3a429874e779e975a6c9a60cbbe24a1dfa3bcc7
ritafuni <amarettoh@gmail.com>
2020-09-01 23:50:29 +0900

    made basic radio buttons for routines

--- src/Components/Routines.jsx
+++ src/Components/Routines.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import { drawerWidth } from './Sidebar';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const Container = styled.div`
   display: flex;
@@ -19,11 +26,21 @@ const Container = styled.div`
   }
 `;
 
+const CustomFormControlLabel = styled(FormControlLabel)`
+  margin-bottom: 0;
+`;
 const Title = styled.h4`
   padding: 8px;
 `;
 
 function Routines(props){
+  const initRoutine = '';
+  const [routine, setRoutine] = React.useState(initRoutine);
+
+  function ChangeRoutine(event){
+    setRoutine(event.target.value);
+  }
+
   return (
     <Container
       drawerOpen={props.drawerOpen}
@@ -32,6 +49,29 @@ function Routines(props){
       <Title>
         ルーチンタスク
       </Title>
+      <FormControl component="fieldset">
+        <Grid container spacing={0}>
+          <Grid container item xs={4} spacing={0} alignContent="flex-start">
+            <FormLabel component="legend">日</FormLabel>
+            <RadioGroup aria-label="day" name="day" value={routine} onChange={ChangeRoutine}>
+              <CustomFormControlLabel value="everyday" control={<Radio />} label="毎日" />
+              <CustomFormControlLabel value="weekday" control={<Radio />} label="平日" />
+            </RadioGroup>
+          </Grid>
+          <Grid container item xs={4} spacing={0} alignContent="flex-start">
+            <FormLabel component="legend">週</FormLabel>
+            <RadioGroup aria-label="week" name="week" value={routine} onChange={ChangeRoutine}>
+              <CustomFormControlLabel value="monday" control={<Radio />} label="月" />
+              <CustomFormControlLabel value="tuesday" control={<Radio />} label="火" />
+              <CustomFormControlLabel value="wednesday" control={<Radio />} label="水" />
+              <CustomFormControlLabel value="thursday" control={<Radio />} label="木" />
+              <CustomFormControlLabel value="friday" control={<Radio />} label="金" />
+              <CustomFormControlLabel value="saturday" control={<Radio />} label="土" />
+              <CustomFormControlLabel value="sunday" control={<Radio />} label="日" />
+            </RadioGroup>
+          </Grid>
+        </Grid>
+      </FormControl>
     </Container>
   );
 }
